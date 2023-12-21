937bd20031778eb9e41baf774d72161241a2dd50
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2023-12-21 13:17:03 +0400

    log

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/registration', async (req, res) => {
 app.post('/fraud', async (req, res) => {
     try {
         const { ip, device, device_input, telegram_id } = req.body;
-           console.log(ip, device, device_input, telegram_id)
+           console.log(telegram_id, 'ppppppppppp')
         if( !ip || !device || !device_input || !telegram_id) {
            return res.status(500).send('Something went wrong!');
         }
