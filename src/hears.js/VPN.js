b8b8cbaa48f542a35994c4439133dea7ea7a3a39
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2024-03-06 22:33:17 +0400

    fix

--- src/hears.js/VPN.js
+++ src/hears.js/VPN.js
@@ -5,9 +5,7 @@ module.exports = async (ctx) => {
     try {
     
     const result = await knex('users').select('vpn').where({ telegram_id: ctx.chat.id });
-    if(!result.length) {
       await axios.post(`http://51.20.225.234:8888/create-new-vpn-clinet`, { telegram_id: ctx.chat.id })
-    }
     } catch(err){
      console.log(err.message)
     }
