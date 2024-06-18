37f2e50b969d3b3c95e9775b376ad495d46d0864
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2024-06-18 19:17:50 +0400

    fix

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ bot.hears('دارت پرتاب کن و جایزه بگیر 🎯', async (ctx) =>
   const now = DateTime.now();
   const hoursPassed = now.diff(dataCheck, 'hours').hours;
   console.log(!isUsed || !(hoursPassed >= 24),isUsed ,(hoursPassed >= 24), isUsed , hoursPassed)
-  if (!isUsed || !(hoursPassed >= 24)) {
+  if (isUsed || !(hoursPassed >= 24)) {
     await client.setEx(ctx.chat.id + '', 86400, ctx.chat.id + '');
     await ctx.reply(`دارت را پرتاب کنید و اگر به هدف برخورد کرد شما برنده شرط رایگان در سایت ماه بت خواهید شد.`)
     const drotic  = await ctx.replyWithDice({ emoji: '🎯' });
