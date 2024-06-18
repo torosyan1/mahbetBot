e641065b0d04c66f7aaad6264f21494c340d081d
Vahag Torosyan <vahag.torosyan@nrgaming.com>
2024-06-18 16:48:42 +0400

    fix

--- server.js
+++ server.js
@@ -84,8 +84,8 @@ bot.hears('دارت پرتاب کن و جایزه بگیر 🎯', async (ctx) =>
 
   const isUsed = await client.get(ctx.chat.id + '');
   const latestRecordQuery = await knex('promo_codes').select('codes', 'active', 'created_at').where('telegram_id', ctx.chat.id + '').orderBy('created_at', 'desc').first();
-  if(!latestRecordQuery?.created_at) {
-    latestRecordQuery?.created_at = DateTime.now();
+  if(!latestRecordQuery.created_at) {
+    latestRecordQuery.created_at = DateTime.now();
   }
   const inputDateTime = DateTime.fromFormat(DateTime.fromISO(latestRecordQuery.created_at).toFormat('yyyy-MM-dd HH:mm:ss'), 'yyyy-MM-dd HH:mm:ss');
   const now = DateTime.now();
@@ -113,8 +113,8 @@ bot.hears('دارت پرتاب کن و جایزه بگیر 🎯', async (ctx) =>
 
   setTimeout(async () => {
     const latestRecordQuery = await knex('promo_codes').select('codes', 'active', 'created_at').where('telegram_id', ctx.chat.id + '').orderBy('created_at', 'desc').first();
-    if(!latestRecordQuery?.created_at) {
-      latestRecordQuery?.created_at = DateTime.now();
+    if(!latestRecordQuery.created_at) {
+      latestRecordQuery.created_at = DateTime.now();
     }
     const inputDateTime = DateTime.fromFormat(DateTime.fromISO(latestRecordQuery.created_at).toFormat('yyyy-MM-dd HH:mm:ss'), 'yyyy-MM-dd HH:mm:ss');
     const now = DateTime.now();
